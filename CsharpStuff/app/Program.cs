7fcd99affdf335bfc95594a0279e176fc8232ac7
generatedscript <61459573+GeneratedScript-RL@users.noreply.github.com>
2022-10-13 04:03:59 -0700

    Add files via upload

---
+++ CsharpStuff/app/Program.cs
@@ -0,0 +1,25 @@
+﻿using Raylib_cs;
+
+namespace HelloWorld
+{
+    static class Program
+    {
+        public static void Main()
+        {
+            string Score = "0";
+            Raylib.InitWindow(800, 480, "Hello World");
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.WHITE);
+
+                Raylib.DrawText("SCORE: " + Score, 330, 20, 30, Color.BLACK);
+
+                Raylib.EndDrawing();
+            }
+
+            Raylib.CloseWindow();
+        }
+    }
+}
